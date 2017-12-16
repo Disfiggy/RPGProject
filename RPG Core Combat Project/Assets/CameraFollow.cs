5cf4c62763c001b29468ee0b39bedc19bdedc789
Disfigure <eero.lamminmaki@gmail.com>
2017-12-16 21:09:05 +0200

    First commit. Created a scene, added some standard assets like character. Character can move on the ground, camera follow is done and cameraraycasting works. Now different layers are logged when cursor hits them.

---
+++ RPG Core Combat Project/Assets/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject player;
+
+
+	// Use this for initialization
+	void Start () {
+
+        // Defining the player variable in the start by calling gameobjects and finding the GameObject with specific tag, in this case the Player, which is a third person character
+        player = GameObject.FindGameObjectWithTag("Player");
+        
+        // For debugging and to just make Unity print the Player gameobject
+        print(player.ToString());
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        // Because the script is attached to the Camera arm, we take the transform position of the camera and set it to be the same as the players transform position.
+        transform.position = player.transform.position;
+
+	}
+}
