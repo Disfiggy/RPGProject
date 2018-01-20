f7864ee1e8cc29d547928aa02d7d88a570ccd7fa
Disfigure <eero.lamminmaki@gmail.com>
2018-01-20 18:25:48 +0200

    Made the project structure a bit cleaner. Added cursor icons and states so when hovering over enemies and other areas, appropriate cursor is used for them.

--- RPG Core Combat Project/Assets/Scripts/PlayerMovement.cs
+++ RPG Core Combat Project/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,8 @@ using UnityStandardAssets.Characters.ThirdPerson;
 [RequireComponent(typeof (ThirdPersonCharacter))]
 public class PlayerMovement : MonoBehaviour
 {
+    bool isInDirectMode = false; // TODO consider making static later
+
     [SerializeField] float walkMoveStopRadius = 0.2f;
 
     ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
@@ -18,9 +20,43 @@ public class PlayerMovement : MonoBehaviour
         currentClickTarget = transform.position;
     }
 
+    // TODO fix issue with click to move and WSD conflicting and increasing speed
+
     // Fixed update is called in sync with physics
     private void FixedUpdate()
     {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            // G for gamepad. TODO allow player to map later
+            isInDirectMode = !isInDirectMode; // toggle mode
+        }
+        if (isInDirectMode)
+        {
+            // ProcessDirectMovement();
+        }
+        else
+        {
+            // ProcessMouseMovement();
+        }
+
+        ProcessIndirectMovement(); // Mouse movement
+    }
+
+    private void ProcessDirectMovement()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+
+        Vector3 m_CamForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 m_Move = v * m_CamForward + h * (Camera.main.transform.right);
+
+        m_Character.Move(Vector3.zero, false, false);
+    }
+
+    private void ProcessIndirectMovement()
+    {
+   
         if (Input.GetMouseButton(0))
         {
             print("Cursor raycast hit " + cameraRaycaster.layerHit);
