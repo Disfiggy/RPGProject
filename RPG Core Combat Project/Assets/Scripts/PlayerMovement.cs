854894ec7377d6ef3b67c8dcc0be0aa6320fd9fc
Disfigure <eero.lamminmaki@gmail.com>
2018-01-21 16:16:15 +0200

    Few edits to the scene. Changed cursor sizes to 32. Can now properly change controls from keyboard to controller with G-key.

--- RPG Core Combat Project/Assets/Scripts/PlayerMovement.cs
+++ RPG Core Combat Project/Assets/Scripts/PlayerMovement.cs
@@ -9,18 +9,16 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] float walkMoveStopRadius = 0.2f;
 
-    ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
+    ThirdPersonCharacter thirdPersonCharacter;   // A reference to the ThirdPersonCharacter on the object
     CameraRaycaster cameraRaycaster;
     Vector3 currentClickTarget;
         
     private void Start()
     {
         cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
-        m_Character = GetComponent<ThirdPersonCharacter>();
+        thirdPersonCharacter = GetComponent<ThirdPersonCharacter>();
         currentClickTarget = transform.position;
     }
-
-    // TODO fix issue with click to move and WSD conflicting and increasing speed
 
     // Fixed update is called in sync with physics
     private void FixedUpdate()
@@ -29,38 +27,40 @@ public class PlayerMovement : MonoBehaviour
         {
             // G for gamepad. TODO allow player to map later
             isInDirectMode = !isInDirectMode; // toggle mode
+            currentClickTarget = transform.position; // clearing the click target
         }
         if (isInDirectMode)
         {
-            // ProcessDirectMovement();
+            ProcessDirectMovement();
         }
         else
         {
-            // ProcessMouseMovement();
+            ProcessMouseMovement();
         }
 
-        ProcessIndirectMovement(); // Mouse movement
+ 
     }
 
     private void ProcessDirectMovement()
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+        
+        Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 movement = v * cameraForward + h * (Camera.main.transform.right);
 
+        thirdPersonCharacter.Move(movement, false, false);
 
-        Vector3 m_CamForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
-        Vector3 m_Move = v * m_CamForward + h * (Camera.main.transform.right);
-
-        m_Character.Move(Vector3.zero, false, false);
+        print("We are in direct movement" + h + v);
     }
 
-    private void ProcessIndirectMovement()
+    private void ProcessMouseMovement()
     {
    
         if (Input.GetMouseButton(0))
         {
-            print("Cursor raycast hit " + cameraRaycaster.layerHit);
-            switch (cameraRaycaster.layerHit)
+            print("Cursor raycast hit " + cameraRaycaster.currentlayerHit);
+            switch (cameraRaycaster.currentlayerHit)
             {
                 case Layer.Walkable:
                     currentClickTarget = cameraRaycaster.hit.point;
@@ -78,11 +78,11 @@ public class PlayerMovement : MonoBehaviour
         var playerToClickPoint = currentClickTarget - transform.position;
         if (playerToClickPoint.magnitude >= walkMoveStopRadius)
         {
-            m_Character.Move(currentClickTarget - transform.position, false, false);
+            thirdPersonCharacter.Move(currentClickTarget - transform.position, false, false);
         }
         else
         {
-            m_Character.Move(Vector3.zero, false, false);
+            thirdPersonCharacter.Move(Vector3.zero, false, false);
         }
     }
 }
